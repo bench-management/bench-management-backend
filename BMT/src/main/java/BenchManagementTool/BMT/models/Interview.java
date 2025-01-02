88222c1ad53a022b12a23296fa13fa4e72823ab5
AdityaPahuja <aditya.pahuja@bounteous.com>
2025-01-02 13:29:03 +0530

    add : validation for Interview

--- BMT/src/main/java/BenchManagementTool/BMT/models/Interview.java
+++ BMT/src/main/java/BenchManagementTool/BMT/models/Interview.java
@@ -2,6 +2,10 @@ package BenchManagementTool.BMT.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -14,15 +18,34 @@ public class Interview {
     @Id
     private String interviewId; // Unique identifier for the interview
 
+    @NotBlank(message="Client id cannot be blank")
     private String clientId; // Client associated with the interview
+
+    @NotNull(message = "interview status must be specified")
     private InterviewStatus interviewStatus; // Enum for the interview status
+
+    @NotBlank(message = "Interviewer name cannot be blank")
+    @NotNull(message = "interview name must be specified")
     private String interviewerName; // Name of the interviewer
+
+    @NotBlank(message = "Project cannot be blank")
     private String project; // Associated project
+
+    @NotBlank(message = "Client requirement cannot be blank")
     private String clientRequirement; // Client's requirement details
+
+    @NotBlank(message = "Accolite hiring manager cannot be blank")
     private String accoliteHiringManager; // Accolite hiring manager
+
+    @NotBlank(message = "Client hiring manager cannot be blank")
     private String clientHiringManager; // Client hiring manager
+
+    @NotBlank(message = "Department cannot be blank")
     private String department; // Associated department
+
+    @NotEmpty(message = "Comments cannot be empty")
     private List<String> comments; // List of comments for the interview
+
 
     public enum InterviewStatus {
         SCHEDULED,
