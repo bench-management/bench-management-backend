23c5a9b4a7b12b8fe847553958546ba708f5885f
tanishq <tanishq.chouhan@bounteous.com>
2025-01-04 17:32:28 +0530

    add : libs package and enums

---
+++ BMT/src/main/java/BenchManagementTool/BMT/libs/Utils.java
@@ -0,0 +1,22 @@
+package BenchManagementTool.BMT.libs;
+
+public class Utils {
+    public enum Skill {
+        JAVA, SPRING_BOOT, ANGULAR, REACT, MONGODB, MYSQL, OTHER
+    }
+
+    public enum Location {
+        MUMBAI, BANGALORE, CHENNAI, HYDERABAD, GURGAON, NCR, OTHER
+    }
+
+    public enum Status {
+        RESIGNED, SABBATICAL, UNDER_EVALUATION, ONBOARDING_IN_PROGRESS, ONBOARDED, INTERVIEW_IN_PROGRESS
+    }
+
+    public enum InterviewStatus {
+        SCHEDULED,
+        ONGOING,
+        REJECTED,
+        SELECTED
+    }
+}
